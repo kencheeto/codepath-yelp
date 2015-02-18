3c7ae7ee8f6e1b6c1b72bcf452e4ab2bbd397e7b
Kenshiro Nakagawa <kencheeto@gmail.com>
2015-02-17 23:53:46 -0800

    extract protocol conformance to class extensions

--- codepath-yelp/FiltersViewController.swift
+++ codepath-yelp/FiltersViewController.swift
@@ -16,29 +16,21 @@ protocol FiltersViewControllerDelegate : class {
   func filtersViewController(viewController: UIViewController, didApplyFilters filters: [String:String])
 }
 
-final class FiltersViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, FilterCellDelegate {
+final class FiltersViewController: UIViewController, UITableViewDataSource {
   
   @IBOutlet private weak var tableView: UITableView!
-  
+
   weak var delegate: FiltersViewControllerDelegate?
+  
   private var filters: [String:String] = [:]
   private let categories = ["ramen", "sushi", "yakitori"]
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    tableView.delegate = self
     tableView.dataSource = self
     
     navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Cancel", style: UIBarButtonItemStyle.Plain, target: self, action: "onCancelButton")
     navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Apply", style: UIBarButtonItemStyle.Plain, target: self, action: "onApplyButton")
-    
-    
-    // Do any additional setup after loading the view.
-  }
-  
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
   }
   
   func onCancelButton() {
@@ -50,20 +42,26 @@ final class FiltersViewController: UIViewController, UITableViewDelegate, UITabl
     dismissViewControllerAnimated(true, completion: nil)
   }
   
+  internal func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return categories.count
+  }
+  
+}
+
+// MARK: - FilterCellDelegate
+extension FiltersViewController: FilterCellDelegate {
+  internal func filterCell(cell: UITableViewCell, didSetFilter filter: (String,String)) {
+    let (type, value) = filter
+    filters[type] = value
+  }
+}
+
+// MARK: - UITableViewDataSource
+extension FiltersViewController: UITableViewDataSource {
   internal func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
     var cell = tableView.dequeueReusableCellWithIdentifier("CategoryFilterCell") as CategoryFilterCell
     cell.categoryLabel.text = categories[indexPath.row]
     cell.delegate = self
     return cell
   }
-  
-  internal func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return categories.count
-  }
-  
-  internal func filterCell(cell: UITableViewCell, didSetFilter filter: (String,String)) {
-    let (type, value) = filter
-    filters[type] = value
-  }
-  
 }
